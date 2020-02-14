53e9e1181aa977227c73078d4fadfd7525cd0665
Frankie Primerano <frankieprimerano@gmail.com>
2020-02-14 11:20:13 -0500

    Key size restraint

--- app.py
+++ app.py
@@ -38,6 +38,10 @@ def upload_file():
     file_data = file.read()
     if len(file_data) <= 8:
         return "File is too small."
+    elif len(key) < 4:
+        return "Key is too short. Must be between 4 and 56 characters."
+    elif len(key) > 56:
+        return "Key is too long. Must be between 4 and 56 characters."
 
     cipher = Encryption(key) 
     encrypted_file_data, iv = cipher.encrypt(file_data)
