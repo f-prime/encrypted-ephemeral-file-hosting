96c622ebc5d2f991a887c125e796bc25ee55e407
Frankie Primerano <frankieprimerano@gmail.com>
2020-02-14 11:27:27 -0500

    Black formatted

--- app.py
+++ app.py
@@ -10,13 +10,21 @@ from flask import Flask, render_template, request, send_file
 from encryption import Encryption
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = "uploads/"
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024 
+app.config["UPLOAD_FOLDER"] = "uploads/"
+app.config["MAX_CONTENT_LENGTH"] = 100 * 1024 * 1024
 
 redis_cli = redis.Redis()
 
+
 def generate_random_id():
-    keygen = lambda x: ''.join([random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(x)])
+    keygen = lambda x: "".join(
+        [
+            random.choice(
+                string.ascii_uppercase + string.ascii_lowercase + string.digits
+            )
+            for x in range(x)
+        ]
+    )
     key = keygen(8)
 
     while redis_cli.get(key):
@@ -24,15 +32,16 @@ def generate_random_id():
 
     return key
 
-@app.route("/", methods=['POST'])
+
+@app.route("/", methods=["POST"])
 def upload_file():
-    if 'file' not in request.files:
+    if "file" not in request.files:
         return "No file provided"
 
     elif "key" not in request.form:
         return "No key provided"
 
-    file = request.files['file']
+    file = request.files["file"]
     key = request.form["key"]
 
     file_data = file.read()
@@ -43,19 +52,19 @@ def upload_file():
     elif len(key) > 56:
         return "Key is too long. Must be between 4 and 56 characters."
 
-    cipher = Encryption(key) 
+    cipher = Encryption(key)
     encrypted_file_data, iv = cipher.encrypt(file_data)
 
     name = file.filename.split(".")
-    
+
     if len(name) == 1:
-        extension = ''
+        extension = ""
     else:
-        extension = '.'.join(name[1:])
+        extension = ".".join(name[1:])
 
     file_id = f"{uuid.uuid4().hex}.{extension}"
 
-    with open(os.path.join(app.config['UPLOAD_FOLDER'], file_id), 'wb') as f:
+    with open(os.path.join(app.config["UPLOAD_FOLDER"], file_id), "wb") as f:
         f.write(encrypted_file_data)
 
     id = generate_random_id()
@@ -65,32 +74,33 @@ def upload_file():
 
     return f"https://trimly.fm/{id}"
 
-@app.route("/", methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-@app.route("/<file>", methods=['POST'])
+
+@app.route("/<file>", methods=["POST"])
 def download(file):
     if "key" not in request.form:
         return "No key provided"
 
-    key = request.form['key']
+    key = request.form["key"]
 
     name = redis_cli.get(file)
     key_hash = redis_cli.get(f"{file}-key")
     iv = redis_cli.get(f"{file}-iv")
 
-
     if not name:
         return "No file with that ID exists"
-    
+
     elif hashlib.sha512(key.encode()).hexdigest() != key_hash.decode():
         return "Invalid key"
 
-    with open(os.path.join(app.config['UPLOAD_FOLDER'], name.decode()), 'rb') as resp:
+    with open(os.path.join(app.config["UPLOAD_FOLDER"], name.decode()), "rb") as resp:
         encrypted_file_data = resp.read()
-        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], name.decode()))
-    
+        os.remove(os.path.join(app.config["UPLOAD_FOLDER"], name.decode()))
+
         cipher = Encryption(key)
         file_data = cipher.decrypt(encrypted_file_data, iv)
 
@@ -99,10 +109,9 @@ def download(file):
         redis_cli.delete(f"{file}-key")
 
         return send_file(
-                BytesIO(file_data),
-                as_attachment=True,
-                attachment_filename=name.decode()
+            BytesIO(file_data), as_attachment=True, attachment_filename=name.decode()
         )
-    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- encryption.py
+++ encryption.py
@@ -1,6 +1,7 @@
 import blowfish
 import os
 
+
 class Encryption(object):
     def __init__(self, key):
         self.key = key
@@ -11,11 +12,11 @@ class Encryption(object):
     def encrypt(self, data):
         cipher = self.create_cipher()
         iv = self.gen_iv()
-        return b''.join(cipher.encrypt_cbc_cts(data, iv)), iv
+        return b"".join(cipher.encrypt_cbc_cts(data, iv)), iv
 
     def decrypt(self, data, iv):
         cipher = self.create_cipher()
-        return b''.join(cipher.decrypt_cbc_cts(data, iv))
+        return b"".join(cipher.decrypt_cbc_cts(data, iv))
 
     def gen_iv(self):
         return os.urandom(8)
